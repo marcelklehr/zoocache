2ce305a37f87c0b37a723e67e84d75d67247eae8
Marcel Klehr <marcel.klehr@gmx.de>
2011-11-04 11:22:45 +0100

    added exception.php

--- zoo.php
+++ zoo.php
@@ -4,5 +4,6 @@ define('ZOOCACHE_VER','3.0.0');
 include ZOOCACHE_INC.'/class/driver.interface.php';
 include ZOOCACHE_INC.'/class/cache.class.php';
 include ZOOCACHE_INC.'/class/config.class.php';
+include ZOOCACHE_INC.'/class/exception.php';
 include ZOOCACHE_INC.'/options.php';
 ?>
