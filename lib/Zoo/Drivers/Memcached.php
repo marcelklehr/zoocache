6150e94fe9923808a76d2e0d4053792b253245d4
Marcel Klehr <mklehr@gmx.net>
2012-06-13 15:32:59 +0200

    Switch to MIT License

--- lib/Zoo/Drivers/Memcached.php
+++ lib/Zoo/Drivers/Memcached.php
@@ -1,22 +1,31 @@
 <?php
 /**
- *  Zoocache - Intelligent, neatly integrating, easily extenable output caching tool
- *  Copyright (C) 2011  Marcel Klehr <marcel.klehr@gmx.de>
- *
- *  This program is free software; you can redistribute it and/or modify it under the 
- *  terms of the GNU General Public License as published by the Free Software Foundation;
- *  either version 3 of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along with this program;
- *  if not, see <http://www.gnu.org/licenses/>.
+ * Zoocache - Intelligent output caching
+ * Copyright (c) 2011-2012 by Marcel Klehr <mklehr@gmx.net>
+ * 
+ * MIT LICENSE
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  * @package Zoocache
  * @author Marcel Klehr <mklehr@gmx.net>
- * @copyright (c) 2011, Marcel Klehr
+ * @copyright (c) 2011-2012, Marcel Klehr
  */
 namespace Zoo\Drivers;
 use \Zoo\Config;
